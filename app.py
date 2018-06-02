154430703acc66dffac25f5f017c697210095572
jchae438 <jchae438@gmail.com>
2018-06-01 23:36:09 -0400

    added dates and more error checking. modal cannot be closed on enter anymore

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pymongo
 import jinja2
 import os
 import pytz
+import dateutil.parser
 
 
 app = Flask(__name__)
@@ -15,15 +16,18 @@ entries = db['entries']
 @app.route("/", methods=['GET','POST'])
 def main():
 	delete_expired_entries();
-	return render_template('index.html', entries=entries.find().sort("start_time", pymongo.ASCENDING), num_entries=entries.count())
+	return render_template('index.html', entries=entries.find().sort([("start_date", pymongo.ASCENDING), ("start_time", pymongo.ASCENDING)]), num_entries=entries.count())
 
 @app.route("/add_entry", methods=['GET','POST'])
 def add_entry():
+	print("beepboop")
 	name = request.form.get("name")
 	phone_number = request.form.get("phone_number")
 	num_swipes = request.form.get("num_swipes")
 	location = request.form.get("location")
+	start_date = request.form.get("start_date")
 	start_time = request.form.get("start_time")
+	end_date = request.form.get("end_date")
 	end_time = request.form.get("end_time")
 	
 	entries.insert_one(
@@ -32,18 +36,25 @@ def add_entry():
 	"phone_number":phone_number,
 	"num_swipes":num_swipes,
 	"location":location,
+	"start_date":start_date,
 	"start_time":start_time,
+	"end_date":end_date,
 	"end_time":end_time
 	})
-
+	print("beepboop2")
 	return redirect('/');
 
 
 def delete_expired_entries():
-	curr_time = datetime.now(pytz.timezone('America/New_York')).time()
+	curr_time = datetime.now(pytz.timezone('America/New_York'))
+	curr_time = curr_time.replace(tzinfo=pytz.utc)
 	for entry in entries.find():
-		end_time = datetime.strptime(entry["end_time"], "%H:%M").time()
-		if curr_time > end_time:
+		end = dateutil.parser.parse(entry["end_date"] + " " + entry["end_time"])
+		end = end.replace(tzinfo=pytz.utc)
+		print("current time: " + str(curr_time))
+		print("end time: " + str(end))
+		if curr_time > end:
+			print("we made it binch")
 			entries.delete_one({'_id':entry['_id']})
 
 
